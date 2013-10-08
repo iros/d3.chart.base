3f07d8e2727e984cab4280ba4e5ef277972fea66
Irene Ros <imirene@gmail.com>
2013-10-08 12:17:02 -0400

    Changing from throttle to denounce.

--- src/base.js
+++ src/base.js
@@ -48,43 +48,30 @@
   //     http://underscorejs.org
   //     (c) 2009-2013 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
   //     Underscore may be freely distributed under the MIT license.
-  // Returns a function, that, when invoked, will only be triggered at most once
-  // during a given window of time. Normally, the throttled function will run
-  // as much as it can, without ever going more than once per `wait` duration;
-  // but if you'd like to disable the execution on the leading edge, pass
-  // `{leading: false}`. To disable execution on the trailing edge, ditto.
-  function _throttle(func, wait, options) {
-    // var timeoutId = -1;
-    // return function() {
-    //   if (timeoutId > -1) {
-    //     window.clearTimeout(timeoutId);
-    //   }
-    //   timeoutId = window.setTimeout(fn, timeout);
-    // };
-
-    var context, args, result;
-    var timeout = null;
-    var previous = 0;
-    options = options || {};
-    var later = function() {
-      previous = options.leading === false ? 0 : new Date();
-      timeout = null;
-      result = func.apply(context, args);
-    };
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+  function _debounce(func, wait, immediate) {
+    var timeout, args, context, timestamp, result;
     return function() {
-      var now = new Date();
-      if (!previous && options.leading === false) { previous = now; }
-      var remaining = wait - (now - previous);
       context = this;
       args = arguments;
-      if (remaining <= 0) {
-        clearTimeout(timeout);
-        timeout = null;
-        previous = now;
-        result = func.apply(context, args);
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
+      timestamp = new Date();
+      var later = function() {
+        var last = (new Date()) - timestamp;
+        if (last < wait) {
+          timeout = setTimeout(later, wait - last);
+        } else {
+          timeout = null;
+          if (!immediate) result = func.apply(context, args);
+        }
+      };
+      var callNow = immediate && !timeout;
+      if (!timeout) {
+        timeout = setTimeout(later, wait);
       }
+      if (callNow) result = func.apply(context, args);
       return result;
     };
   }
